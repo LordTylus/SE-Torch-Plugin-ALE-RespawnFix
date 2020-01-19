80101cd770bdecc8803af2e0c1fcb7102849ae92
LordTylus <LordTylus@users.noreply.github.com>
2020-01-19 14:50:19 +0100

    Initial commit

---
+++ MySpaceRespawnComponentPatch.cs
@@ -0,0 +1,70 @@
+﻿using ALE_Core.Utils;
+using NLog;
+using Sandbox.Game.Entities.Character;
+using Sandbox.Game.World;
+using SpaceEngineers.Game.World;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Torch.Managers.PatchManager;
+using Torch.Utils;
+using VRage.Utils;
+using VRageMath;
+
+namespace ALE_RespawnFix {
+
+    public class MySpaceRespawnComponentPatch {
+
+        public static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        internal static readonly MethodInfo update =
+            typeof(MySpaceRespawnComponent).GetMethod("GetFriendlyPlayerPositions", BindingFlags.Static | BindingFlags.NonPublic) ??
+            throw new Exception("Failed to find method to patch");
+
+        internal static readonly MethodInfo updatePatch =
+            typeof(MySpaceRespawnComponentPatch).GetMethod(nameof(GetFriendlyPlayerPositionsPatch), BindingFlags.Static | BindingFlags.Public) ??
+            throw new Exception("Failed to find patch method");
+
+        public static void Patch(PatchContext ctx) {
+
+            ReflectedManager.Process(typeof(MySpaceRespawnComponentPatch));
+
+            try {
+
+                ctx.GetPattern(update).Prefixes.Add(updatePatch);
+
+                Log.Info("Patching Successful MySpaceRespawnComponent!");
+
+            } catch (Exception e) {
+                Log.Error(e, "Patching failed!");
+            }
+        }
+
+        public static bool GetFriendlyPlayerPositionsPatch(ref ClearToken<Vector3D> __result, long identityId) {
+
+            var faction = FactionUtils.GetPlayerFaction(identityId);
+            if (faction == null) {
+                __result = new List<Vector3D>().GetClearToken();
+                return false;
+            }
+
+            List<Vector3D> positions = new List<Vector3D>();
+
+            foreach (long member in faction.Members.Keys) {
+
+                MyIdentity identity = PlayerUtils.GetIdentityById(member);
+
+                MyCharacter character = identity.Character;
+                if (character == null || character.IsDead || character.MarkedForClose)
+                    continue;
+
+                positions.Add(character.PositionComp.GetPosition());
+            }
+
+            positions.ShuffleList();
+            __result = positions.GetClearToken();
+
+            return false;
+        }
+    }
+}
---
+++ RespawnFixPlugin.cs
@@ -0,0 +1,53 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Torch;
+using Torch.API;
+using Torch.API.Managers;
+using Torch.API.Session;
+using Torch.Managers.PatchManager;
+using Torch.Session;
+
+namespace ALE_RespawnFix {
+
+    public class RespawnFixPlugin : TorchPluginBase {
+
+        public static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        private PatchManager patchManager;
+        private PatchContext ctx;
+
+        public override void Init(ITorchBase torch) {
+            
+            base.Init(torch);
+
+            var sessionManager = Torch.Managers.GetManager<TorchSessionManager>();
+            if (sessionManager != null)
+                sessionManager.SessionStateChanged += SessionChanged;
+            else
+                Log.Warn("No session manager loaded!");
+
+            patchManager = Torch.Managers.GetManager<PatchManager>();
+            if (patchManager != null) {
+
+                if (ctx == null)
+                    ctx = patchManager.AcquireContext();
+
+            } else {
+                Log.Warn("No patch manager loaded!");
+            }
+        }
+
+        private void SessionChanged(ITorchSession session, TorchSessionState newState) {
+
+            if (newState == TorchSessionState.Loaded) {
+
+                MySpaceRespawnComponentPatch.Patch(ctx);
+                patchManager.Commit();
+            }
+        }
+    }
+}
